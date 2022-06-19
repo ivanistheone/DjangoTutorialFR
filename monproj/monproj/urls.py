138ff3a35d14c602b4151940dc65f566b5e71870
Ivan Savov <ivan.savov@gmail.com>
2022-06-19 10:13:06 -0400

    Added basic HTML template rendering

--- monproj/monproj/urls.py
+++ monproj/monproj/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from monsite.views import hello
+from monsite.views import homepage
 
 urlpatterns = [
+    url(r'^', homepage),
     url(r'^hello', hello),
     url(r'^admin/', admin.site.urls),
 ]
--- monproj/monsite/views.py
+++ monproj/monsite/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import get_template
 
 # Create your views here.
 
 def hello(request):
     msg = "Salut Montréal!"
     return HttpResponse(msg)
+
+def homepage(request):
+    template = get_template("base.html")
+    html = template.render()
+    return HttpResponse(html)
